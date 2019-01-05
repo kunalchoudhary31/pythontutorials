e28e2311c1d343a957b2c70f12d7fcb3045ed454
k.choudhary@salesbee.io-at-382933657370 <k.choudhary@salesbee.io>
2019-01-05 09:01:21 +0530

    First file

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#print("Hello World!")
+#print("Hello Again")
+print("I like typing this")
+print("This is fun") #Life sucks
+
+print('Yay! printing')
+print("I'd much rather you 'not'.")
+print('I had "said" do not touch this.')
+print("I'd rather do this than''' that")
